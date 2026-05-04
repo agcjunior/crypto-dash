1885e7ab47650074ef417f7b3c0d163f1eb431a8
antonio.junior_wipro <agcjunior@gmail.com>
2026-05-04 15:54:12 -0300

    Set cards limit to show

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import CoinCard from "./components/CoinCard";
+import LimitSelector from "./components/LimitSelector";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -7,11 +8,14 @@ const App = () => {
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  
+  const [limit, setLimit] = useState(10);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`${API_URL}&order=market_cap_desc&per_page=10&page=1&sparkline=false`);
+        const response = await fetch(
+          `${API_URL}&order=market_cap_desc&per_page=${limit}&page=1&sparkline=false`,
+        );
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
@@ -25,16 +29,19 @@ const App = () => {
     };
 
     fetchData();
-  }, []);
+  }, [limit]);
 
   return (
     <div>
       <h1>Crypto Dash</h1>
       {loading && <p>Loading...</p>}
       {error && <p>Error: {error}</p>}
+
+      <LimitSelector limit={limit} setLimit={setLimit} /> 
+
       {!loading && !error && (
         <main className="grid">
-          {coins.map(coin => (
+          {coins.map((coin) => (
             <CoinCard key={coin.id} coin={coin} />
           ))}
         </main>
